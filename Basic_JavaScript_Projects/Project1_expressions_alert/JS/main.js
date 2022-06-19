7bacad9fca342e486d5ba51ba80609cdb56e8c99
Rshalom <77251628+Rshalom@users.noreply.github.com>
2022-06-19 14:51:09 -0400

    Practice using alerts, expressions, and statements

---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,31 @@
+window.alert("Hello, World!")
+document.write("I am currently using JavaScript to write a message!");
+document.write("<br>") // Adding a line break
+var x = "This is a string assigned to a variable" // Assigning a string value to a variable
+document.write(x); // Printing the variable
+document.write("<br>")
+
+var y = "Another string variable"
+window.alert(y); // Creating an alert through assigning a variable
+
+document.write("Pratice using quotation marks: \"\" and apostrophes \'."); // escaping quotations using backslashes
+document.write("<br>")
+document.write("This is a result of two strings " + "connected together."); // Concatenating strings
+document.write("<br>")
+
+var a = "Concatenated string" + " assigned to a variable." // Assigning a concatenated string to a variable
+
+document.write(a); 
+document.write("<br>")
+
+var name = "Rebecca", age = "twenty-two", height = "five feet-seven inches" , spouse = "Eliyahu"
+document.write(spouse); // Assigning multiple variables in one statement
+document.write("<br>")
+
+var expression = 100 * 20
+document.write(expression); // Writing and printing an expression
+document.write("<br>")
+
+var m = "First string"
+var n = " and second string" 
+document.write(m + n); // Assigning two variables to two strings and concatenating them together
